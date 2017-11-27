7ef97cd1c10386e34ab12bbfd8d98f42428e0b92
Manikandan Elangovan <manoelangoo@gmail.com>
2017-11-27 23:32:31 +0530

    Add changes in EntryDetai for styling.

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import { months } from 'moment';
+import { View, Text, StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
+import { timeToString, getDailyRemainderValue } from '../utils/helpers'
+import MetricCard from './MetricCard'
+import { white } from '../utils/colors'
+import TextButton from './TextButton'
+import { addEntry } from '../actions'
+import { removeEntry } from '../utils/api'
 
 class EntryDetail extends Component {
     static navigationOptions = ({ navigation }) => {
@@ -11,16 +17,59 @@ class EntryDetail extends Component {
         const day = entryId.slice(8)
 
         return {
-            title: `${Month}/${day}/${year}`
+            title: `${month}/${day}/${year}`
         }
     }
+    reset = () => {
+        const { remove, goBack, entryId } = this.props
+
+        remove()
+        goBack()
+        removeEntry(entryId)
+    }
+    shouldComponentUpdate (nextProps) {
+        return nextProps.metrics != null && !nextProps.metrics.today
+    }
+
     render() {
+        const { metrics } = this.props
         return (
-            <View>
-                <Text>Entry Detail - {JSON.stringify(this.porps.navigation.state.params.entryId)}</Text>
+            <View style={styles.container}>
+                <MetricCard metrics={metrics} />
+                <TextButton style={{margin: 20}} onPress={this.reset}>RESET</TextButton>
             </View>
         )
     }
 }
 
-export default EntryDetail
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        backgroundColor: white,
+        padding: 15,
+    },
+})
+
+function mapStateToProps (state, {navigation}){
+    const {entryId} = navigation.state.params
+
+    return {
+        entryId,
+        metrics: state[entryId],
+    }
+}
+
+function mapDispatchToProps( dispatch, { navigation }) {
+    const { entryId } = navigation.state.params
+
+    return {
+        remove: () => dispatch(addEntry({
+            [entryId] : timeToString() === entryId
+                ? getDailyRemainderValue()
+                : null
+        })),
+        goBack: () => navigation.goBack(),
+    }
+}
+
+export default connect (mapStateToProps, mapDispatchToProps)(EntryDetail)
